efe0ba03d6762893ff84cb62600eb50608e1d9bb
kusin <aryajayaalamsyah@gmail.com>
2022-12-26 21:57:37 +0700

    -

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ import numpy as np;
 # library data visualization
 import plotly.express as px;
 
+# import function any file .py
+from func_acq_data import *;
+
 # set config ui-dasboard streamlit
 st.set_page_config(
     page_title="My Dasboard",
@@ -26,14 +29,138 @@ st.set_page_config(
 # --------------------------------------------------------------------------------------- #
 # data acquisition ---------------------------------------------------------------------- #
 # --------------------------------------------------------------------------------------- #
+# load dataset
+dataset = pd.read_csv("dataset/fire_archive_M-C61_317736.csv");
 
-# container-header-fuild
-st.markdown("## Prediction hotspot with enso and rainfall factor used LSTM-RNN");
-st.markdown("- reference dataset from https://firms.modaps.eosdis.nasa.gov/download/");
 
-st.metric(
-    label="Percentage recovery",
-    value="{:.2f}".format((dataset["daily_recovery"].sum() / dataset["daily_positive"].sum())*100)+"%", 
-    delta="0,35%"
-);
+# container-header
+with st.container():
+    st.markdown("## Prediction hotspot with enso and rainfall factor used LSTM-RNN and GRU-RNN");
+    st.write("The impact of enso on increasing the number of hotspots");
 
+
+# container-summary of impact enso to hotspot
+with st.container():
+    
+    # define col-4 row-1
+    col1, col2, col3, col4 = st.columns(4);
+    
+    # col year 2006
+    col1.metric(
+        label="Year 2006",
+        value="154,985 point", 
+        delta="0,00%"
+    );
+    
+    # col year 2009
+    col2.metric(
+        label="Year 2009",
+        value="102344 point", 
+        delta="0,00%"
+    );
+    
+    # col year 2015
+    col3.metric(
+        label="Year 2015",
+        value="178,578 point", 
+        delta="0,00%"
+    );
+    
+    # col year 2019
+    col4.metric(
+        label="Year 2019",
+        value="90,475 point", 
+        delta="0,00%"
+    );
+
+
+# container-hotspot-indonesia
+with st.container():
+    
+    # call dataset karhutla indonesia
+    df_karhutla = func_karhutla();
+    
+    # set sub-header
+    st.write("Data visualization hotspot indonesia");
+    
+    # define columns with col-2 row-1
+    col1, col2 = st.columns(2);
+    
+    # columns col-1
+    with col1:
+        
+        # with pie-plot
+        st.plotly_chart(
+            px.pie(
+                df_karhutla, 
+                values="area", 
+                names="year", 
+                color="year",
+                hole=0.5,
+                color_discrete_sequence=["#9FC9C9", "#B4DBD4", "#F2F2C2", "#EEE8AE", "#F7D05E", "#9FC9C9"]
+            ),
+            use_container_width=True
+        );
+    
+    # columns col-2
+    with col2:
+        
+        # with bar-plot
+        st.plotly_chart(
+            px.bar(
+                df_karhutla,
+                x="year",
+                y="area",
+                color_discrete_sequence=["#ffbf40"],
+                text_auto=".2s"
+            ),
+            use_container_width=True
+        );
+
+
+# container-hotspot-indonesia with eda
+with st.container():
+    
+    # set sub-header
+    st.write("Data visualization hotspot indonesia");
+    
+    # set tab-index
+    tab1, tab2 = st.tabs(["with line-plot", "with bar-plot"]);
+    
+    # tab-index-1
+    with tab1:
+        
+        # call dataset hotspot indonesia
+        df_hospot = func_agg(dataset, "acq_date", "hotspot", "D");
+        
+        # set time-series-plot
+        st.plotly_chart(
+            px.line(
+                df_hospot,
+                x=df_hospot.index,
+                y="hotspot",
+                color_discrete_sequence=["#3457D5"]
+            ),
+            use_container_width=True
+        );
+    
+    # tab-index-2
+    with tab2:
+        
+        # call dataset hotspot indonesia
+        df_hospot = func_agg(dataset, "acq_date", "hotspot", "Y");
+        
+        # set barplot
+        st.plotly_chart(
+            px.bar(
+                df_hospot,
+                x=df_hospot.index,
+                y="hotspot",
+                color_discrete_sequence=["#3457D5"],
+                text_auto=".2s"
+            ),
+            use_container_width=True
+        );
+    
+    
+    
---
+++ func_acq_data.py
@@ -0,0 +1,58 @@
+import pandas as pd;
+import numpy as np;
+import streamlit as st;
+
+
+# func load dataset Indonesia country
+@st.cache(allow_output_mutation=True)
+def df_country(dataset):
+    
+    # statment
+    
+    # return value
+    return dataset;
+
+
+# func load dataset province South Sumatra
+@st.cache(allow_output_mutation=True)
+def df_province(dataset):
+    
+    # statment
+    
+    # return value
+    return dataset;
+
+
+# func aggregate dataset hotspot
+@st.cache(allow_output_mutation=True)
+def func_agg(data, nm_group="", nm_index="", nm_agg=""):
+    
+    # groupby data by columns and count each rows
+    data = data.groupby([nm_group]).size().reset_index(name=nm_index);
+    
+    # set index datetime
+    data = data.set_index(pd.to_datetime(data[nm_group]));
+
+    # resample mountly
+    data = data.resample(nm_agg).sum();
+    
+    #return value
+    return data;
+
+
+# func load dataset karhutla
+@st.cache(allow_output_mutation=True)
+def func_karhutla():
+    
+    # the data karhutla 2014 - 2019
+    data={
+        "year" : ["2014", "2015", "2016", "2017", "2018", "2019"],
+        "area" : ["44411", "2611411", "438363", "165484", "529267", "1649258"]
+    };
+    
+    # convert data to dataframe
+    data = pd.DataFrame(data);
+    
+    #return value
+    return data;
+
